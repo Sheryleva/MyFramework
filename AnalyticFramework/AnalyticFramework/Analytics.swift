d8cd0d27cc7e8b8f48e7b33576c50e828fd26cc6
Sheryl Evangelene Pulikandala <67703990+Sheryleva@users.noreply.github.com>
2020-10-16 18:20:32 -0400

    Add files via upload

---
+++ AnalyticFramework/AnalyticFramework/Analytics.swift
@@ -0,0 +1,16 @@
+//
+//  Analytics.swift
+//  AnalyticFramework
+//
+//  Created by Sheryl Evangelene Pulikandala on 10/16/20.
+//
+
+import Foundation
+
+public class Analytics {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
